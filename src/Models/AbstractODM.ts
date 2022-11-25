4fec36325a75eb0688f6bdf27b3e00e882f3b1da
camillageber <camilla.geber@gmail.com>
2022-11-25 05:00:25 +0000

    feat: add methods to find cars

--- src/Models/AbstractODM.ts
+++ src/Models/AbstractODM.ts
@@ -1,4 +1,5 @@
-import { Model, models, model, Schema } from 'mongoose';
+import { Model, models, model, Schema, isValidObjectId } from 'mongoose';
+import UnprocessableEntityError from '../errors/UnprocessableEntityError';
 
 abstract class AbstractODM<T> {
   protected model: Model<T>;
@@ -13,6 +14,15 @@ abstract class AbstractODM<T> {
 
   public async create(obj: T): Promise<T> {
     return this.model.create({ ...obj });
+  }
+
+  public async findAll(): Promise<T[]> {
+    return this.model.find({});
+  }
+
+  public async findById(_id: string): Promise<T | null> {
+    if (!isValidObjectId(_id)) throw new UnprocessableEntityError('Invalid mongo id');
+    return this.model.findById({ _id });
   }
 }
 
--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -1,20 +1,30 @@
 import Car from '../Domains/Car';
+import NotFoundError from '../errors/NotFoundError';
 import ICar from '../Interfaces/ICar';
 import CarODM from '../Models/CarODM';
 
 class CarService {
-  private createCarDomain(car: ICar | null): Car | null {
-    if (car) {
-      return new Car(car);
-    }
-    return null;
+  private createCarDomain(car: ICar): Car {
+    return new Car(car);
   }
-
+  
   public async createCar(car: ICar) {
     const carODM = new CarODM();
     const newCar = await carODM.create(car);
     return this.createCarDomain(newCar);
   }
-}
 
+  public async findAllCars(): Promise<Car[]> {
+    const carODM = new CarODM();
+    const findCars = await carODM.findAll();
+    return findCars.map((car) => this.createCarDomain(car));
+  }
+
+  public async findCarById(id: string) {
+    const carODM = new CarODM();
+    const car = await carODM.findById(id);
+    if (!car) throw new NotFoundError('Car not found');
+    return this.createCarDomain(car);
+  }
+}
 export default CarService;
